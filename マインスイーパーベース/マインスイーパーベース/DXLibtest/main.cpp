81469731fc4b44134217ff7482e147bb90e46635
MSI\GA2B <h1202415020@hiratagakuen.onmicrosoft.com>
2026-02-03 10:04:50 +0900

    no message

--- マインスイーパーベース/マインスイーパーベース/DXLibtest/main.cpp
+++ マインスイーパーベース/マインスイーパーベース/DXLibtest/main.cpp
@@ -1,6 +1,6 @@
 #include "DxLib.h"
 #include <time.h>
-#include "saiki.h"
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine, _In_ int nShowCmd)
@@ -15,16 +15,23 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance,
 
 	SetDrawScreen(DX_SCREEN_BACK); //描画先を裏画面に変更
 	SetWindowText("MineSweeper"); //ウィンドウの名前
-
-
+	
 	//画像---------------------
 	int gh_back;//背景
 
 	//画像読み込み
 	gh_back = LoadGraph("image\\back.png");
-	
-	Board board;
 
+	struct Cell
+	{
+		int turf;
+		int empty;
+		int x;
+		int y;
+		bool IsClick = false;
+		bool IsFlag = false;
+	};
+	
 	//変数
 	int r = rand();
 	int BOM_MAX = 30;
@@ -36,57 +43,41 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance,
 	bool start = false;
 	int mouseX, mouseY;//カーソル位置保存用
 
-	while (1) 
+	int empty = LoadGraph("image\\empty.png");
+	int one = LoadGraph("image\\one.png");
+	int two = LoadGraph("image\\two.png");
+	int three = LoadGraph("image\\three.png");
+	int four = LoadGraph("image\\four.png");
+	int five = LoadGraph("image\\five.png");
+	int six = LoadGraph("image\\six.png");
+	int seven = LoadGraph("image\\seven.png");
+	int turf = LoadGraph("image\\turf.png");
+	int frag = LoadGraph("image\\frag.png");
+	int mine = LoadGraph("image\\mine.png");
+
+	int placed = 0;
+
+	while (1)
 	{
 		//裏画面のデータを全て削除
 		ClearDrawScreen();
 
 		//処理----------------------------------------------------------------
-		board.Click();
-
-
-
-
-
-		for (int i = 0; i < BOM_MAX; i++)
 		{
-			B_randX = rand() % 15;
-			B_randY = rand() % 12;
-			for (int y = 0; y < Bom_Count; y++)
-			{
-				if (bomX[y] == B_randX && bomY[y] == B_randY)
-				{
-					BOM_MAX++;
-					continue;
-				}
-				else
-				{
-					bomX[Bom_Count] = B_randX;
-					bomY[Bom_Count] = B_randY;
-					Bom_Count++;
-				}
-			}
+			//--------------------------------------------------------------------
+
+			ScreenFlip(); //裏画面データを表画面へ反映
+
+			//毎ループ呼び出す。エラーになった場合breakする
+			if (ProcessMessage() == -1)break;
+			//エスケープキーを押したり、エラーになった場合、breakする
+			if (CheckHitKey(KEY_INPUT_ESCAPE))break;
 		}
+		//画像データ削除
+		DeleteGraph(gh_back);
 
-	
-		//画像の描画(位置X、位置Y、グラフィックハンドル、透明度の有効無効)
-		//背景
-		DrawGraph(0, 0, gh_back, true);
-		board.Draw();
-		//--------------------------------------------------------------------
-
-		ScreenFlip(); //裏画面データを表画面へ反映
-
-		//毎ループ呼び出す。エラーになった場合breakする
-		if (ProcessMessage() == -1)break;
-		//エスケープキーを押したり、エラーになった場合、breakする
-		if (CheckHitKey(KEY_INPUT_ESCAPE))break;
+		WaitKey();	 //キー入力待ち
+		DxLib_End(); //DXライブラリ使用の終了処理
+		return 0;
 	}
-
-	//画像データ削除
-	DeleteGraph(gh_back);
-
-	WaitKey();	 //キー入力待ち
-	DxLib_End(); //DXライブラリ使用の終了処理
-	return 0;
 }
